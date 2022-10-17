9a021e755b20e089958bc2ff3fe1e89ebba841f5
Davincci <kodeaap@users.noreply.github.com>
2022-10-17 12:00:39 +0700

    Fix SessionStorage Issue

--- src/hooks/useFeatureFlag.ts
+++ src/hooks/useFeatureFlag.ts
@@ -2,7 +2,7 @@ import { FLAG_FARM } from 'config/flag'
 import { Atom, useAtomValue } from 'jotai'
 import { atomWithStorage, createJSONStorage } from 'jotai/utils'
 
-const storage = createJSONStorage(() => sessionStorage)
+const storage = typeof window !== 'undefined' ? createJSONStorage(() => sessionStorage) : null
 export const featureFarmApiAtom = atomWithStorage<typeof FLAG_FARM>(
   'feature-farm-api',
   FLAG_FARM,
